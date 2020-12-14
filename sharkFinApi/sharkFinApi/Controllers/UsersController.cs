bd320afddffd4e0b2536d8d5294da8f0d0090e68
mattmarnien <mgm215@gmail.com>
2020-12-14 16:11:30 -0500

    fleshed out user api routes

--- sharkFinApi/sharkFinApi/Controllers/UsersController.cs
+++ sharkFinApi/sharkFinApi/Controllers/UsersController.cs
@@ -11,36 +11,67 @@ namespace sharkFinApi.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+
+        //returns just holders for the moment
         [HttpGet]
         public IActionResult GetUsers()
         {
             return StatusCode(200);
         }
+
+        [HttpPost]
+        public IActionResult AddUser()
+        {
+            return StatusCode(201);
+        }
+
         [HttpGet("{id}")]
         public IActionResult GetUserByID()
         {
             return StatusCode(200);
         }
-        [HttpGet("{id}/portfolios")]
-        public IActionResult GetUserPortfolios()
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id)
         {
             return StatusCode(200);
         }
 
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            return StatusCode(200);
+        }
+
+        [HttpGet("{id}/portfolios")]
+        public IActionResult GetUserPortfolios(int id)
+        {
+            return StatusCode(200);
+        }
+        [HttpPost("{id}/portfolios")]
+        public IActionResult AddPortfolio(int id)
+        {
+            return StatusCode(201);
+        }
+
         [HttpGet("{id}/portfolios/{id}")]
-        public IActionResult GetPortfolioById()
+        public IActionResult GetPortfolioById(int userId, int portId)
+        {
+            return StatusCode(200);
+        }
+        [HttpPut("{id}/portfolios/{id}")]
+        public IActionResult UpdatePortfolio(int userId, int portId)
         {
             return StatusCode(200);
         }
 
         [HttpGet("{id}/portfolios/{id}/trades")]
-        public IActionResult GetTrades()
+        public IActionResult GetTrades(int userId, int portId)
         {
             return StatusCode(200);
         }
 
         [HttpGet("{id}/potfolios/{id}/trades/{id}")]
-        public IActionResult GetTradeById()
+        public IActionResult GetTradeById(int userId, int portId, int tradeId)
         {
             return StatusCode(200);
         }
