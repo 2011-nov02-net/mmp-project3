d49b6c67805576b94bffe8f050f378565b94724c
Matthew Goodman <mgoodman934@gmail.com>
2020-12-14 17:20:34 -0500

    Add base domain model classes, repository interfaces

--- sharkFinApi/DataAccess/Models/Portfolio.cs
+++ sharkFinApi/DataAccess/Models/Portfolio.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 
 #nullable disable
 
-namespace DataAccess
+namespace DataAccess.Models
 {
-    public partial class Stock
+    public partial class Portfolio
     {
-        public Stock()
+        public Portfolio()
         {
             PortfolioEntries = new HashSet<PortfolioEntry>();
             Trades = new HashSet<Trade>();
         }
 
-        public string Symbol { get; set; }
-        public string Market { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
-        public string Logo { get; set; }
+        public int UserId { get; set; }
+        public decimal Funds { get; set; }
 
+        public virtual User User { get; set; }
         public virtual ICollection<PortfolioEntry> PortfolioEntries { get; set; }
         public virtual ICollection<Trade> Trades { get; set; }
     }
--- sharkFinApi/DataAccess/Models/PortfolioEntry.cs
+++ sharkFinApi/DataAccess/Models/PortfolioEntry.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 
 #nullable disable
 
-namespace DataAccess
+namespace DataAccess.Models
 {
-    public partial class Trade
+    public partial class PortfolioEntry
     {
-        public int Id { get; set; }
         public int PortfolioId { get; set; }
         public string StockSymbol { get; set; }
         public string StockMarket { get; set; }
         public int Quantity { get; set; }
-        public decimal Price { get; set; }
-        public DateTime TimeTraded { get; set; }
 
         public virtual Portfolio Portfolio { get; set; }
         public virtual Stock Stock { get; set; }
