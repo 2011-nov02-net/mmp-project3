4b06f110629fac469dea82f3c2960d2dfa52c45b
Paul Cortez <cortezpaul19@gmail.com>
2020-12-14 15:44:54 -0600

    Added bare structure for classes, interface and repos

---
+++ sharkFinApi/Stock Library/PortfolioEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock_Library
+{
+    public class PortfolioEntry
+    {
+
+    }
+}
---
+++ sharkFinApi/Stock Library/Stocks.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock_Library
+{
+    public class Stocks
+    {
+    }
+}
