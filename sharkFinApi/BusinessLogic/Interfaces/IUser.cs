155c51e1d0d69c1bf4c28842d5a6706d79ce9895
Paul Cortez <cortezpaul19@gmail.com>
2020-12-14 16:27:49 -0600

    Updated IUser

--- sharkFinApi/BusinessLogic/Interfaces/IUser.cs
+++ sharkFinApi/BusinessLogic/Interfaces/IUser.cs
@@ -10,9 +10,17 @@ namespace BusinessLogic.Interfaces
         public IEnumerable<Stock> GetStocks();
         public IEnumerable<Stock> GetStockByName(string name);
         public IEnumerable<User> GetUserLeading(int id);
+        public IEnumerable<Portfolio> GetPortfolios();
+        public IEnumerable<Portfolio> GetPortfolioById(int id);
+
 
         public void AddUser(User user);
         public void AddPortfolio(PortfolioEntry portfolioEntry);
         public void SellStocks(Portfolio portfolio);
+        public void EditUser(User user);
+        public void UpdateUser(User user);
+        public void DeleteUser(User user);
+        public void AddFund(Portfolio portfolio);
+        public void AddEntry(PortfolioEntry portfolioEntry);
     }
 }
