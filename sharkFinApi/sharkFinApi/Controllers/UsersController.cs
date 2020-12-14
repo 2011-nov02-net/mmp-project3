7af26193d0de2d61421d5372f61b62bd90afe972
mattmarnien <mgm215@gmail.com>
2020-12-14 15:41:05 -0500

    started api routes, got rid of default weather forecast files

---
+++ sharkFinApi/sharkFinApi/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sharkFinApi.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            return StatusCode(200);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetUserByID()
+        {
+            return StatusCode(200);
+        }
+        [HttpGet("{id}/portfolios")]
+        public IActionResult GetUserPortfolios()
+        {
+            return StatusCode(200);
+        }
+
+        [HttpGet("{id}/portfolios/{id}")]
+        public IActionResult GetPortfolioById()
+        {
+            return StatusCode(200);
+        }
+
+        [HttpGet("{id}/portfolios/{id}/trades")]
+        public IActionResult GetTrades()
+        {
+            return StatusCode(200);
+        }
+
+        [HttpGet("{id}/potfolios/{id}/trades/{id}")]
+        public IActionResult GetTradeById()
+        {
+            return StatusCode(200);
+        }
+    }
+}
