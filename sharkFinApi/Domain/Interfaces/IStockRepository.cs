6fa1a8364f633b72b9f959f0b2c7832b06def55b
Paul Cortez <cortezpaul19@gmail.com>
2020-12-16 17:01:44 -0600

    Modified IUser to be async

--- sharkFinApi/Domain/Interfaces/IStockRepository.cs
+++ sharkFinApi/Domain/Interfaces/IStockRepository.cs
@@ -8,8 +8,8 @@ namespace Domain.Interfaces {
     public interface IStockRepository {
         
         Task<IEnumerable<Stock>> GetAll();
-        Task<Stock> GetAllBySymbol(string symbol);
-        Task<Stock> GetAllByMarket(string market);
+        Task<IEnumerable<Stock>> GetStockBySymbol(string symbol);
+        Task<IEnumerable<Stock>> GetStockByMarket(string market);
         Task<Stock> GetOneStock(string symbol, string market);
         Task Add(Stock stock);
         Task Update(Stock stock);
--- sharkFinApi/Domain/Interfaces/IUserRepository.cs
+++ sharkFinApi/Domain/Interfaces/IUserRepository.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Domain.Models;
 
 namespace Domain.Interfaces {
     public interface IUserRepository {
-        ICollection<User> GetAll();
-        ICollection<User> GetAll(string firstName, string lastName);
-        User Get(int id);
-        User Get(string email);
-        void Add(User user);
-        void Update(User user);
-        void Delete(User user);
+        
+        Task<IEnumerable<User>> GetAll();
+        Task<User> GetUserByName(string firstName, string lastName);
+        Task<User> GetUserById(int id);
+        Task<User> GetUserByEmail(string email);
+        Task Add(User user);
+        Task Update(User user);
+        Task Delete(User user);
     }
 }
