c635c7d77a73e24ead543edf74b0b6755eba8c14
mattmarnien <mgm215@gmail.com>
2020-12-21 13:26:27 -0500

    began laying out front end framework, services, and calls to backend

--- sharkFin/src/app/app-routing.module.ts
+++ sharkFin/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { OktaCallbackComponent} from '@okta/okta-angular';
+import { OktaCallbackComponent, OktaAuthGuard} from '@okta/okta-angular';
 import {HomeComponent} from './home/home.component';
+import {UserComponent} from './user/user.component';
 const routes: Routes = [
   {
     path: '',
@@ -10,6 +11,10 @@ const routes: Routes = [
   {
     path: 'login/callback',
     component: OktaCallbackComponent
+  },{
+    path: 'user',
+    canActivate: [OktaAuthGuard],
+    component: UserComponent
   }
 ];
 
