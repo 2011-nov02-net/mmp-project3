eb609fdb8df31a23565f13f7c50b076be98e3b29
Paul Cortez <cortezpaul19@gmail.com>
2020-12-22 15:58:59 -0600

    Added UserRepo Test

--- sharkFinApi/XUnitTest/UserRepoTest.cs
+++ sharkFinApi/XUnitTest/UserRepoTest.cs
@@ -1,13 +1,117 @@
-﻿using System;
+﻿using DataAccess.Models;
+using DataAccess.Repositories;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
+using Domain.Models;
+
 
 namespace XUnitTest
 {
-    public class UserRepoTest
+    public partial class UnitTest
     {
+        Domain.Models.User testUser;
+        [Fact]
+        public async Task AddCustomer_Database_TestAsync()
+        {
+            using var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+            var options = new DbContextOptionsBuilder<mmpproject2Context>().UseSqlite(connection).Options;
+            testUser = new Domain.Models.User
+            {
+                FirstName = "Grace",
+                LastName = "Libardos",
+                Email = "gl@gmail.com",
+                UserName = "gl001"
+            };
+
+            using (var context = new mmpproject2Context(options))
+            {
+                context.Database.EnsureCreated();
+                var repo = new UserRepository(context, new NullLogger<UserRepository>());
+
+                await repo.AddAsync(testUser);
+            }
+
+            using var context2 = new mmpproject2Context(options);
+            DataAccess.Models.User userReal = context2.Users
+                .Single(l => l.FirstName == "Grace");
+
+            Assert.Equal(testUser.FirstName, userReal.FirstName);
+            Assert.Equal(testUser.Email, userReal.Email);
+        }
+        [Fact]
+        public async Task GetUsers_Database_testAsync()
+        {
+            using var connection = Database_init();
+            var options = new DbContextOptionsBuilder<mmpproject2Context>().UseSqlite(connection).Options;
+            using var context = new mmpproject2Context(options);
+            var repo = new UserRepository(context, new NullLogger<UserRepository>());
+
+            var users = await repo.GetAllAsync();
+            var usersActual = context.Users.ToList();
+
+            foreach (var user in users)
+            {
+                Assert.Contains(user.FirstName, usersActual.Select(x => x.FirstName));
+                Assert.Contains(user.LastName, usersActual.Select(x => x.LastName));
+                Assert.Contains(user.Email, usersActual.Select(x => x.Email));
+                Assert.Contains(user.UserName, usersActual.Select(x => x.UserName));
+            }
+        }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public async Task GetUserbyID_Database_test(int id)
+        {
+            using var connection = Database_init();
+            var options = new DbContextOptionsBuilder<mmpproject2Context>().UseSqlite(connection).Options;
+            using var context = new mmpproject2Context(options);
+            var repo = new UserRepository(context, new NullLogger<UserRepository>());
+
+            var user = await repo.GetAsync(id);
+
+            var userActual = context.Users.Where(x => x.Id == id).Single();
+
+            Assert.Equal(user.Id, userActual.Id);
+            Assert.Equal(user.FirstName, userActual.FirstName);
+            Assert.Equal(user.LastName, userActual.LastName);
+            Assert.Equal(user.Email, userActual.Email);
+            Assert.Equal(user.UserName, userActual.UserName);
+
+        }
+
+        [Theory]
+        [InlineData("rosel@gmail.com")]
+        [InlineData("rody@gmail.com")]
+        [InlineData("graceLibardos@gmail.com")]
+        [InlineData("joseRizal@gmail.com")]
+        public async Task GetUserbyEmail_Database_test(string email)
+        {
+            using var connection = Database_init();
+            var options = new DbContextOptionsBuilder<mmpproject2Context>().UseSqlite(connection).Options;
+            using var context = new mmpproject2Context(options);
+            var repo = new UserRepository(context, new NullLogger<UserRepository>());
+
+            var user = await repo.GetAsync(email);
+
+            var userActual = context.Users.Where(x => x.Email == email).Single();
+
+            Assert.Equal(user.Id, userActual.Id);
+            Assert.Equal(user.FirstName, userActual.FirstName);
+            Assert.Equal(user.LastName, userActual.LastName);
+            Assert.Equal(user.Email, userActual.Email);
+            Assert.Equal(user.UserName, userActual.UserName);
+        }
+
 
     }
 }
