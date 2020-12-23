2b1ef141e270bc43303e5618fa3ef1beee2f79f4
Paul Cortez <cortezpaul19@gmail.com>
2020-12-22 18:49:51 -0600

    Fixed portfolio bug

--- sharkFinApi/Domain/Models/Portfolio.cs
+++ sharkFinApi/Domain/Models/Portfolio.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Domain.Models {
-    public class Portfolio {
+namespace Domain.Models
+{
+    public class Portfolio
+    {
 
         private readonly ICollection<Asset> _assets;
         public IReadOnlyCollection<Asset> Assets => new HashSet<Asset>(_assets);
@@ -15,14 +17,17 @@ namespace Domain.Models {
         public string Name { get; set; }
         public decimal Funds { get; set; }
 
+        public Portfolio()
+        {
+
         }
-    public Portfolio(string name, decimal funds, ICollection<Asset> assets, ICollection<Trade> trades)
-    {
-        Name = name;
-        Funds = funds;
-        _assets = assets ?? new HashSet<Asset>();
-        _trades = trades ?? new HashSet<Trade>();
-    }
+        public Portfolio(string name, decimal funds, ICollection<Asset> assets, ICollection<Trade> trades)
+        {
+            Name = name;
+            Funds = funds;
+            _assets = assets ?? new HashSet<Asset>();
+            _trades = trades ?? new HashSet<Trade>();
+        }
 
         // TODO:
         //  - Implement portfolio-related methods such as adding/deleting assets and trades
