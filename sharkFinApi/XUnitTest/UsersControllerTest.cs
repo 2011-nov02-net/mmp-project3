1eb731a1b21ccef827456231f89b2d36b76f58d8
Paul Cortez <59485329+froggypirates22@users.noreply.github.com>
2020-12-23 23:31:45 -0600

    Update UsersControllerTest.cs
    That should fixed the errors

--- sharkFinApi/XUnitTest/UsersControllerTest.cs
+++ sharkFinApi/XUnitTest/UsersControllerTest.cs
@@ -1,33 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xunit;
-using Moq;
-using AutoMapper;
 using DataAccess;
 using sharkFinApi.Controllers;
 using Domain.Interfaces;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging.Abstractions;
 
 namespace XUnitTest
 {
     public class UsersControllerTest
     {
 
-        static readonly Mock<IUserRepository> _mockRepo = new Mock<IUserRepository>();
-        static readonly UsersController usersController = new UsersController(_mockRepo.Object, null, new NullLogger<UsersController>());
-        string fake = "fakeId";
-        string fake2 = "fakeId2";
     }
-    //[Fact]
-    //public async Task UserController_GetAllUsers()
-   // {
-     
-
-        
-    }
-
 }
